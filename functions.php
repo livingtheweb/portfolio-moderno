99cabc8e480e2cd9e2699d00d745f8b7b5e7376e
Mariano <livingtheweb01@gmail.com>
2020-09-25 12:19:43 -0300

    Agregamos functions y page-templates

--- functions.php
+++ functions.php
@@ -1,8 +1,19 @@
 <?php 
 
   // Funciones nativas de WordPress
+  add_theme_support('post-thumbnails');
 
   // Funciones propias del usuarios
-  
+  // function fecha() {
+  //   $fecha =  Date('Y');
+  //   echo $fecha;
+   
+  // }
+
+  function sarasa() {
+    $fecha =  Date('Y');
+    echo $fecha;
+  }
+
 
 
---
+++ page-templates/template-servicios.php
@@ -0,0 +1,7 @@
+<?php 
+
+//  Template name: Servicios
+
+?>
+
+<h2>Soy el template para mostrar el contenido de la página Servicios</h2>
